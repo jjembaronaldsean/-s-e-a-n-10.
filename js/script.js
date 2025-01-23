4b87b2d8f8d36f4454fdff49623e947f2ad8f594
jjemba ronald #s̸e̸a̸n̸ 10. <134681128+jjembaronaldsean@users.noreply.github.com>
2025-01-23 19:44:17 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
 }
+
+
+
+// START OF VIEWER COUNT
+ // Initialize viewerCount 
+  let viewerCount = 0; 
+
+  // Check if there's a stored count in local storage
+  if (localStorage.getItem('viewerCount')) {
+    viewerCount = parseInt(localStorage.getItem('viewerCount'));
+    document.getElementById("viewer-count").textContent = viewerCount; 
+  }
+
+  // Update viewer count on page load
+  updateViewerCount();
+
+  // Function to update the viewer count
+  function updateViewerCount() {
+    viewerCount++;
+    document.getElementById("viewer-count").textContent = viewerCount;
+    localStorage.setItem('viewerCount', viewerCount); 
+  }
+
+  // You can remove this if you don't need real-time updates
+  // setInterval(updateViewerCount, 1000); // Update every second 
+// END OF VIEWER COUNT
